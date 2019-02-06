3ac393ffad32cea453da9923b3c2573145802f5d
Mritunjay Kumar <mritunjay.kumar@rackspace.com>
2019-02-06 14:12:33 -0800

    Fixed in TenantRoutes class.

--- src/main/java/com/rackspacecloud/metrics/ingestionservice/influxdb/providers/DevTestTenantRouteProvider.java
+++ src/main/java/com/rackspacecloud/metrics/ingestionservice/influxdb/providers/DevTestTenantRouteProvider.java
@@ -1,64 +1,68 @@
 package com.rackspacecloud.metrics.ingestionservice.influxdb.providers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
+import java.io.IOException;
 
 public class DevTestTenantRouteProvider implements RouteProvider {
+    private final static String ROUTES = "{\n" +
+            "  \"tenantId\": \"hybrid_1667601\",\n" +
+            "  \"routes\": {\n" +
+            "    \"60m\": {\n" +
+            "      \"path\": \"http://data-influxdb:8086\",\n" +
+            "      \"databaseName\": \"db_hybrid_1667601\",\n" +
+            "      \"retentionPolicyName\": \"rp_155d\",\n" +
+            "      \"retentionPolicy\": \"155d\",\n" +
+            "      \"maxSeriesCount\": 10000\n" +
+            "    },\n" +
+            "    \"240m\": {\n" +
+            "      \"path\": \"http://data-influxdb:8086\",\n" +
+            "      \"databaseName\": \"db_hybrid_1667601\",\n" +
+            "      \"retentionPolicyName\": \"rp_300d\",\n" +
+            "      \"retentionPolicy\": \"300d\",\n" +
+            "      \"maxSeriesCount\": 10000\n" +
+            "    },\n" +
+            "    \"full\": {\n" +
+            "      \"path\": \"http://data-influxdb:8086\",\n" +
+            "      \"databaseName\": \"db_hybrid_1667601\",\n" +
+            "      \"retentionPolicyName\": \"rp_5d\",\n" +
+            "      \"retentionPolicy\": \"5d\",\n" +
+            "      \"maxSeriesCount\": 10000\n" +
+            "    },\n" +
+            "    \"1440m\": {\n" +
+            "      \"path\": \"http://data-influxdb:8086\",\n" +
+            "      \"databaseName\": \"db_hybrid_1667601\",\n" +
+            "      \"retentionPolicyName\": \"rp_1825d\",\n" +
+            "      \"retentionPolicy\": \"1825d\",\n" +
+            "      \"maxSeriesCount\": 10000\n" +
+            "    },\n" +
+            "    \"5m\": {\n" +
+            "      \"path\": \"http://data-influxdb:8086\",\n" +
+            "      \"databaseName\": \"db_hybrid_1667601\",\n" +
+            "      \"retentionPolicyName\": \"rp_10d\",\n" +
+            "      \"retentionPolicy\": \"10d\",\n" +
+            "      \"maxSeriesCount\": 10000\n" +
+            "    },\n" +
+            "    \"20m\": {\n" +
+            "      \"path\": \"http://data-influxdb:8086\",\n" +
+            "      \"databaseName\": \"db_hybrid_1667601\",\n" +
+            "      \"retentionPolicyName\": \"rp_20d\",\n" +
+            "      \"retentionPolicy\": \"20d\",\n" +
+            "      \"maxSeriesCount\": 10000\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
+
     @Override
-    public TenantRoutes getRoute(String tenantId, RestTemplate restTemplate) {
-        TenantRoutes tenantRoutes = new TenantRoutes();
-        tenantRoutes.setTenantId(tenantId);
-        Map<String, TenantRoutes.TenantRoute> routes = tenantRoutes.getRoutes();
-
-        routes.put("full", new TenantRoutes.TenantRoute(
-                "http://localhost:8086",
-                "db_hybrid_1667601",
-                "rp_5d",
-                "5d",
-                10000
-        ));
-
-        routes.put("5m", new TenantRoutes.TenantRoute(
-                "http://localhost:8086",
-                "db_hybrid_1667601",
-                "rp_10d",
-                "10d",
-                10000
-        ));
-
-        routes.put("20m", new TenantRoutes.TenantRoute(
-                "http://localhost:8086",
-                "db_hybrid_1667601",
-                "rp_20d",
-                "20d",
-                10000
-        ));
-
-        routes.put("60m", new TenantRoutes.TenantRoute(
-                "http://localhost:8086",
-                "db_hybrid_1667601",
-                "rp_155d",
-                "155d",
-                10000
-        ));
-
-        routes.put("240m", new TenantRoutes.TenantRoute(
-                "http://localhost:8086",
-                "db_hybrid_1667601",
-                "rp_300d",
-                "300d",
-                10000
-        ));
-
-        routes.put("1440m", new TenantRoutes.TenantRoute(
-                "http://localhost:8086",
-                "db_hybrid_1667601",
-                "rp_1825d",
-                "1825d",
-                10000
-        ));
-
-        return tenantRoutes;
+    public TenantRoutes getRoute(String tenantId, RestTemplate restTemplate){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            TenantRoutes routes = mapper.readValue(ROUTES, TenantRoutes.class);
+            return routes;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
