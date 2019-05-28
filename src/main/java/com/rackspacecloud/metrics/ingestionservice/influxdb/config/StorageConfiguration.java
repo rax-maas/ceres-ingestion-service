8e84edcf5d0708476766209f8cd65acaa5cda118
Zack Shoylev <zack.shoylev@gmail.com>
2019-05-28 11:46:58 -0500

    Imports fix

--- src/main/java/com/rackspacecloud/metrics/ingestionservice/influxdb/config/StorageConfiguration.java
+++ src/main/java/com/rackspacecloud/metrics/ingestionservice/influxdb/config/StorageConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 @EnableConfigurationProperties(BackupProperties.class)
