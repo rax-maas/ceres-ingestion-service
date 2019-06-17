4ecc8107a95fe3f224d9de14bf991def2027fac6
Zack Shoylev <zack.shoylev@gmail.com>
2019-06-17 18:45:59 -0500

    Adjust how backup works Remove instance name (same info as db name) Add ability to always flush

--- src/main/java/com/rackspacecloud/metrics/ingestionservice/influxdb/providers/LineProtocolBackupService.java
+++ src/main/java/com/rackspacecloud/metrics/ingestionservice/influxdb/providers/LineProtocolBackupService.java
@@ -18,12 +18,11 @@ public interface LineProtocolBackupService {
     /**
      * A helper method for accessing the backup service
      * @param payload The payload (line protocol payload that can be used for extracting a timestamp)
-     * @param instance Specific database instance this payload is going to (used for bucketing)
      * @param database Database name used for the payload on the database instance
      * @param retentionPolicy Retention policy name on the database instance
      * @throws IOException
      */
-    void writeToBackup(String payload, String instance, String database, String retentionPolicy) throws IOException;
+    void writeToBackup(String payload, String database, String retentionPolicy) throws IOException;
 
     /**
      * Flushes and closes all buffers to the implemented backend and clears any caching.
